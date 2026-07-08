1b223ddffcfb99ae31c14d2dc6f9a32d7ac26c31
engsiam <siam.softwareengineer@gmail.com>
2026-07-08 20:26:10 +0600

    Implement refactoring of core modules for improved performance and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
